5da2de46adfd3b361a7ef03918402ae6d898428a
Black_cat <ichernok@gmail.com>
2020-06-17 10:46:22 +0300

    Homework2

---
+++ src/HomeWork2/App.java
@@ -0,0 +1,21 @@
+package HomeWork2;
+
+public class Demo {
+    public static void main(String[] args) throws MyArraySizeException {
+        String[][] array = new String[5][5];
+        myArray(array);
+    }
+
+    private static void myArray(String[][] a)throws MyArraySizeException {
+        if (a.length < 4 && a.length > 4){
+            throw new MyArraySizeException("Кол-во строк и столбцов в массиве должно" +
+                    " быть равно 4 ");
+            for(x)
+
+            }
+            }
+        }
+
+    }
+
+}
