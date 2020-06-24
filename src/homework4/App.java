f1732884c39be936cb61341279ef6d06294eb48e
Black_cat <ichernok@gmail.com>
2020-06-24 16:44:16 +0300

    Homework4

---
+++ src/homework4/App.java
@@ -0,0 +1,14 @@
+package homework4;
+
+import javax.swing.*;
+
+public class ChatWindow {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MyWindow();
+            }
+        });
+    }
+}
